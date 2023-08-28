8bc5389a5b5074663d157f56ac2c7853388eea5c
victor odambatafwa <omondivictoras@gmail.com>
2023-08-28 12:26:59 +0300

    Completed assignment

--- Review.py
+++ Review.py
@@ -1,3 +1,23 @@
 
 class Review:
-    pass
+    
+    all_reviews = []
+    
+    def __init__(self, customer, restaurant, rating):
+        self._customer = customer
+        self._restaurant = restaurant
+        self._rating = rating
+        Review.all_reviews.append(self)
+        
+    def customer(self):
+        return self._customer
+    
+    def restaurant(self):
+        return self._restaurant
+    
+    def rating(self):
+        return self._rating
+    
+    @classmethod
+    def all(cls):
+        return cls.all_reviews
